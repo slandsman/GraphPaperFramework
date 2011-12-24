6f4e6f7dd6b20dc0695fcd545e4b6bb9b63157cb
Seth Landsman <seth@homeforderangedscientists.net>
2011-12-24 12:11:45 -0500

    initial support for unit tests

---
+++ GraphPaperFrameworkUnitTests/GraphPaperFrameworkUnitTests.h
@@ -0,0 +1,13 @@
+//
+//  GraphPaperFrameworkUnitTests.h
+//  GraphPaperFrameworkUnitTests
+//
+//  Created by Seth Landsman on 12/24/11.
+//  Copyright (c) 2011 MITRE Corporation. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GraphPaperFrameworkUnitTests : SenTestCase
+
+@end
---
+++ GraphPaperFrameworkUnitTests/GraphPaperFrameworkUnitTests.m
@@ -0,0 +1,32 @@
+//
+//  GraphPaperFrameworkUnitTests.m
+//  GraphPaperFrameworkUnitTests
+//
+//  Created by Seth Landsman on 12/24/11.
+//  Copyright (c) 2011 MITRE Corporation. All rights reserved.
+//
+
+#import "GraphPaperFrameworkUnitTests.h"
+
+@implementation GraphPaperFrameworkUnitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GraphPaperFrameworkUnitTests");
+}
+
+@end
