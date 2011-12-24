9c5d07c8d09d645d641b6e1670fae5bbe9040e4b
Seth Landsman <seth@homeforderangedscientists.net>
2011-12-24 12:12:07 -0500

    Initial stubs for unit tests

--- GraphPaperFramework/GraphPaperFramework.h
+++ GraphPaperFramework/GraphPaperFramework.h
@@ -8,6 +8,4 @@
 
 #import <Foundation/Foundation.h>
 
-@interface GraphPaperFramework : NSObject
 
-@end
--- GraphPaperFrameworkUnitTests/GraphPaperFrameworkUnitTests.m
+++ GraphPaperFrameworkUnitTests/GraphPaperFrameworkUnitTests.m
@@ -1,9 +1,9 @@
 //
-//  GraphPaperFrameworkUnitTests.m
-//  GraphPaperFrameworkUnitTests
+//  GraphPaperFrameworkTests.m
+//  GraphPaperFrameworkTests
 //
-//  Created by Seth Landsman on 12/24/11.
-//  Copyright (c) 2011 MITRE Corporation. All rights reserved.
+//  Created by Seth Landsman on 10/3/11.
+//  Copyright (c) 2011 HomeForDerangedScientists. All rights reserved.
 //
 
 #import "GraphPaperFrameworkUnitTests.h"
@@ -26,7 +26,39 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in GraphPaperFrameworkUnitTests");
+    STFail(@"Unit tests are not implemented yet in GraphPaperFrameworkTests");
+}
+
+/**
+ Validate that a simulated click at an empty cell will turn on the cell
+ */
+- (void)testClickOn 
+{
+    
+}
+
+/**
+ Validate that a simulated click at a full cell will turn off the cell
+ */
+-(void)testClickOff
+{
+    
+}
+
+/**
+ Validate that a click off, followed by a click on will toggle a full cell
+ */
+-(void)testToggleOn
+{
+    
+}
+
+/**
+ Validate that a click on, followed by a click off will toggle a full cell
+ */
+-(void)testToggleOff
+{
+    
 }
 
 @end
